27985f24b9b948a921886fac7b5b095808e71031
Mano Sriram <mano.sriram0@gmail.com>
2020-03-03 22:56:13 +0530

    Add Template.

---
+++ Template.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+#include <float.h>
+#include <limits.h>
+#include <stack>
+#include <queue>
+#include <time.h>
+#include "unordered_map"
+using namespace std;
+#define MAX INT_MAX
+#define MIN INT_MIN
+#define INF 1e9
+#define FASTER cin.sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long int
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+int main() {
+    clock_t tStart = clock();
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+
+
+    cout << "Time Taken: " << (double)(clock() - tStart)/CLOCKS_PER_SEC << endl;
+}
